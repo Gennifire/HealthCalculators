9bd3d22391e08e852ec0d25d4b835ff4ca5a2a30
Gennifire <Jenniferdee86@gmail.com>
2022-02-16 14:45:32 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace HealthCalculators
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_BMI_Page = new System.Windows.Forms.Button();
+            this.btn_Macro_Page = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_BMI_result = new System.Windows.Forms.Button();
+            this.age_label = new System.Windows.Forms.Label();
+            this.height_Label = new System.Windows.Forms.Label();
+            this.weight_Label = new System.Windows.Forms.Label();
+            this.BMI_pic = new System.Windows.Forms.PictureBox();
+            this.results_Box = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BMI_pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_BMI_Page
+            // 
+            this.btn_BMI_Page.Location = new System.Drawing.Point(12, 37);
+            this.btn_BMI_Page.Name = "btn_BMI_Page";
+            this.btn_BMI_Page.Size = new System.Drawing.Size(94, 94);
+            this.btn_BMI_Page.TabIndex = 0;
+            this.btn_BMI_Page.Text = "BMI";
+            this.btn_BMI_Page.UseVisualStyleBackColor = true;
+            this.btn_BMI_Page.Click += new System.EventHandler(this.btn_BMI_Page_Click);
+            // 
+            // btn_Macro_Page
+            // 
+            this.btn_Macro_Page.Location = new System.Drawing.Point(12, 130);
+            this.btn_Macro_Page.Name = "btn_Macro_Page";
+            this.btn_Macro_Page.Size = new System.Drawing.Size(94, 94);
+            this.btn_Macro_Page.TabIndex = 1;
+            this.btn_Macro_Page.Text = "Macros";
+            this.btn_Macro_Page.UseVisualStyleBackColor = true;
+            this.btn_Macro_Page.Click += new System.EventHandler(this.btn_Macro_Page_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 94);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 316);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 94);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btn_BMI_result
+            // 
+            this.btn_BMI_result.Location = new System.Drawing.Point(153, 344);
+            this.btn_BMI_result.Name = "btn_BMI_result";
+            this.btn_BMI_result.Size = new System.Drawing.Size(187, 39);
+            this.btn_BMI_result.TabIndex = 4;
+            this.btn_BMI_result.Text = "Get BMI";
+            this.btn_BMI_result.UseVisualStyleBackColor = true;
+            // 
+            // age_label
+            // 
+            this.age_label.AutoSize = true;
+            this.age_label.Location = new System.Drawing.Point(142, 48);
+            this.age_label.Name = "age_label";
+            this.age_label.Size = new System.Drawing.Size(39, 20);
+            this.age_label.TabIndex = 5;
+            this.age_label.Text = "Age:";
+            // 
+            // height_Label
+            // 
+            this.height_Label.AutoSize = true;
+            this.height_Label.Location = new System.Drawing.Point(142, 141);
+            this.height_Label.Name = "height_Label";
+            this.height_Label.Size = new System.Drawing.Size(81, 20);
+            this.height_Label.TabIndex = 6;
+            this.height_Label.Text = "Height (m)";
+            // 
+            // weight_Label
+            // 
+            this.weight_Label.AutoSize = true;
+            this.weight_Label.Location = new System.Drawing.Point(142, 234);
+            this.weight_Label.Name = "weight_Label";
+            this.weight_Label.Size = new System.Drawing.Size(86, 20);
+            this.weight_Label.TabIndex = 7;
+            this.weight_Label.Text = "Weight (kg)";
+            // 
+            // BMI_pic
+            // 
+            this.BMI_pic.Image = ((System.Drawing.Image)(resources.GetObject("BMI_pic.Image")));
+            this.BMI_pic.Location = new System.Drawing.Point(404, 12);
+            this.BMI_pic.Name = "BMI_pic";
+            this.BMI_pic.Size = new System.Drawing.Size(384, 304);
+            this.BMI_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.BMI_pic.TabIndex = 8;
+            this.BMI_pic.TabStop = false;
+            // 
+            // results_Box
+            // 
+            this.results_Box.Location = new System.Drawing.Point(522, 337);
+            this.results_Box.Multiline = true;
+            this.results_Box.Name = "results_Box";
+            this.results_Box.Size = new System.Drawing.Size(241, 79);
+            this.results_Box.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(444, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Results:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.results_Box);
+            this.Controls.Add(this.BMI_pic);
+            this.Controls.Add(this.weight_Label);
+            this.Controls.Add(this.height_Label);
+            this.Controls.Add(this.age_label);
+            this.Controls.Add(this.btn_BMI_result);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_Macro_Page);
+            this.Controls.Add(this.btn_BMI_Page);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.BMI_pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_BMI_Page;
+        private System.Windows.Forms.Button btn_Macro_Page;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_BMI_result;
+        private System.Windows.Forms.Label age_label;
+        private System.Windows.Forms.Label height_Label;
+        private System.Windows.Forms.Label weight_Label;
+        private System.Windows.Forms.PictureBox BMI_pic;
+        private System.Windows.Forms.TextBox results_Box;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthCalculators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_BMI_Page_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Macro_Page_Click(object sender, EventArgs e)
+        {
+            BMI_pic.Visible = false;
+            btn_BMI_result.Visible = false;
+        }
+    }
+}
